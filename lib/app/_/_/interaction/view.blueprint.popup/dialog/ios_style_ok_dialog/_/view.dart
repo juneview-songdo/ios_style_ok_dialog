6334dae5ae38f26d00dd1f983318af57737cc00a
june lee <melodysdreamj@gmail.com>
2024-05-04 15:27:52 +0900

    Initial commit

---
+++ lib/app/_/_/interaction/view.blueprint.popup/dialog/ios_style_ok_dialog/_/view.dart
@@ -0,0 +1,73 @@
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
+import '../../../../../../../../main.dart';
+import '_/state_child.dart';
+import '_/state_mother.dart';
+
+class NewView extends StatefulWidget {
+  NewView({
+    super.key,
+    required this.title,
+    required this.body,
+    this.okText = 'OK',
+    required this.tapOkButton,
+  });
+
+  final String title;
+  final String body;
+  final String okText;
+  final VoidCallback tapOkButton;
+
+  @override
+  State<NewView> createState() => StateChild();
+}
+
+class NewViewState extends State<NewView> with StateMother {
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      constraints: BoxConstraints(maxWidth: 300), // maximum width
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Gap(15),
+          Text("Title")
+              .textStyle(Theme.of(context).textTheme.bodyLarge!)
+              .fontWeight(FontWeight.bold),
+          Gap(5),
+          Text("This is message."),
+          Gap(20),
+          Container()
+              .height(1)
+              .width(double.infinity)
+              .backgroundColor(Colors.grey.withOpacity(0.4)),
+          Container(
+            child: Text("OK")
+                .textStyle(Theme.of(context).textTheme.bodyLarge!)
+                .textColor(Colors.blue.shade700)
+                .fontWeight(FontWeight.bold)
+                .center(),
+          )
+              .paddingDirectional(vertical: 14)
+              .width(double.infinity)
+              .backgroundColor(Colors.transparent)
+              .gestures(onTap: () {
+            widget.tapOkButton();
+          }),
+        ],
+      ),
+    );
+  }
+}
+
+main() async {
+  return buildApp(
+      appHome: NewView(
+    title: 'Title',
+    body: 'This is message.',
+    tapOkButton: () {
+      print('OK');
+    },
+  ).center());
+}
